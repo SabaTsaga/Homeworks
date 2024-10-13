4152af5c7121a86f392bc92590a19ed6a9d85dbc
Saba Tsagareishvili <tsagareishvilisaba0@gmail.com>
2024-10-13 23:17:47 +0400

    Homework N1

---
+++ Homework1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+////// 1. გადაიყვანეთ შემდეგი ათობითი რიცხვები ორობითში:
+////ა) 17
+////ბ) 34
+////გ) 255
+
+//a) 10001
+//b) 100010
+//c)11111111
+
+//2. გადაიყვანეთ შემდეგი ორობითი რიცხვები ათობითში:
+//ა)10101
+//ბ)110011
+//გ)10000000
+
+//a) 21
+//b) 51
+//c) 128
+
+//განმარტეთ რას ნიშნავს ცვლადი C# ში, როგორ შეგვიძლია მისი გამოცხადება და ინიციალიზაცია. მოიყვანეთ რამდენიმე მაგალითი
+
+//cvladi inaxavs mnishvnelobebs monacemebis tavis mexsierebashi. titoeul cvlads aqvs tavisi monacemta tipi, romelic gansazgvravs ra tipis monacemebis shenaxva sheudzlia mas
+//cvladebi aris: integer, string, bool, double, float, decimal.
+//magalitad rom gamovidzaxot integer:
+int age = 21;
+//stringis shemtxvevashi:
+string firstName = "Saba";
+string lastName = "Tsagareishvili";
+
+
